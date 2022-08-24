200434557e697094e3e8a91e39112451f2f6977f
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2022-08-24 20:47:22 +0300

    add tasks and test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,18 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('@nomiclabs/hardhat-etherscan');
-
+require('hardhat-gas-reporter');
+require('./tasks/block-number');
+require('solidity-coverage');
 require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const { RINKEBY_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+const {
+  RINKEBY_RPC_URL = '',
+  PRIVATE_KEY = '',
+  ETHERSCAN_API_KEY = '',
+  COINMARKETCAP_API_KEY = '',
+} = process.env;
 
 module.exports = {
   defaultNetwork: 'hardhat',
@@ -15,9 +22,21 @@ module.exports = {
       accounts: [PRIVATE_KEY],
       chainId: 4, // Rinkeby
     },
+    localhost: {
+      url: 'http://127.0.0.1:8545/',
+      chainId: 31337,
+    },
   },
   solidity: '0.8.8',
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    noColors: true,
+    outputFile: 'gas-report.txt',
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    // token: 'MATIC', // prise in MATIC
   },
 };
